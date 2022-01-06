3e68e6b202e0b298b33b0983aa0ca08e50054412
hanzife <hanzifes@gmail.com>
2022-01-06 16:23:36 +0100

    moulayspot

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
 $(document).ready(function () {
-    //Hover state on Projects Grid
-$( ".GridProject" ).each(function(index) {
+  //Hover state on Projects Grid
+  $( ".GridProject" ).each(function(index) {
 
-        $(this).hover( function(){
-            // What ?
-            $(this).children('.gridOverlay').show();
-            
-            // $(".gridOverlay").css("display", "block");          
-        });
+    $(this).hover( function(){
+        // What ?
+        // $(this).children('.gridOverlay').show();
+        
+        // $(".gridOverlay").css("display", "block");          
+    });
 
-        $(this).mouseleave(function(){
-            $(this).children('.gridOverlay').hide();
-        });
-            
-        });
+    $(this).mouseleave(function(){
+        // $(this).children('.gridOverlay').hide();
+    });
+              
+  });
 
         
         /* ******* */
